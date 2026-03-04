2b3dabab49551132c8d2b7a09dad86a75b06882c
raffmarro97-lab <raffmarro97@gmail.com>
2026-03-04 01:56:54 +0000

    Silver Breweries and utils

--- medallion_dlt_breweries/transformations/02silver_breweries.py
+++ medallion_dlt_breweries/transformations/02silver_breweries.py
@@ -1,13 +1,75 @@
 from pyspark import pipelines as dp
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, count, count_if
+from pyspark.sql import functions as F
+from utilities import utils
 
 # This file defines a sample transformation.
 # Edit the sample below or add new transformations
 # using "+ Add" in the file browser.
 
-@dp.table
-def sample_users_mar_1_1328():
-    return (
-        spark.read.table("samples.wanderbricks.users")
-        .select("user_id", "email", "name", "user_type")
-    )
+@dp.table(
+    name = "silver_brewries",
+    comment = "Cleaning the bronze table and add ingestion_ts"
+)
+def silver_breweries():
+    df = spark.read.table("bronze_brewries")
+    run_ts = spark.sql("SELECT current_timestamp()").collect()[0][0]
+    df =
+        df.select(            
+            'address_1', 
+            'address_2', 
+            'address_3', 
+            'brewery_type', 
+            'city', 
+            'country', 
+            'id', 
+            'latitude', 
+            'longitude', 
+            'name', 
+            'phone', 
+            'postal_code', 
+            'state', 
+            'state_province', 
+            'street', 
+            'website_url'
+        )
+        .where(
+            ( F.col("country") == "United States") &
+            ( F.col("address_1").isNotNull() )
+        )
+        .withColumn(
+            "address_2",
+            utils.fill_null("address_2", "Doesn't exist")
+        )
+        .withColumn(
+            "address_3",
+            utils.fill_null("address_3", "Doesn't exist")
+        )
+        .withColumn(
+            "name",
+            F.regexp_replace(F.col("name"), "Â", "")
+        )
+        .withColumn(
+            "phone",
+            utils.clean_phone(F.col("phone") )
+        )
+        .withColumn(
+            "postalcode",
+            F.when(
+                F.col("postal_code").isNull(),
+                F.lit("Unknown")
+            )
+            .when(
+                F.col("country") == "United States",
+                F.regexp_replace(F.col("postal_code"), "-.*", "")
+            )
+            .otherwise(
+                F.col("postal_code")
+            )
+        )
+        .withColumn(
+            "ingestion_ts", 
+            F.lit(run_ts) #da utlizzare coem sequence_by
+        )
+
+    return df
--- medallion_dlt_breweries/utilities/utils.py
+++ medallion_dlt_breweries/utilities/utils.py
@@ -1,14 +1,28 @@
-from pyspark.sql.functions import udf
-from pyspark.sql.types import BooleanType
-import re
+from pyspark.sql import functions as F
 
-@udf(returnType=BooleanType())
-def is_valid_email(email):
+def clean_phone(col_name):
     """
-    This function checks if the given email address has a valid format using regex.
-    Returns True if valid, False otherwise.
+    This function define to clean number phone and is filtering when the field is Null. Otherwise do a regexp_replace on col_name.
     """
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    if email is None:
-        return False
-    return re.match(pattern, email) is not None
+    return(
+        F.when(
+            F.col(col_name).isNull(), 
+            F.lit("Unknown")
+        )
+        .otherwise(
+            F.regexp_replace(
+                    F.regexp_replace(F.col(col_name), r"^\+\d{1,3}s", ""), 
+                    #^ = inizio stringa; \+ = il simbolo + presente nel prefisso; \d = una cifra; {1,3} = cifra ripetuta 1, 2 o 3 volte; \s = uno spazio
+                    #Trovare una stringa che inizia con il simbolo +, seguito da 1-3 cifre, seguito da uno spazio — e rimuovila
+                    r"[^\d]", ""
+                    #[ ] = Insieme di caratteri; ^ dentro [] = Negazione — "tutto tranne..."; \d = Cifre 0-9
+                    #Trova tutti i caratteri che NON sono cifre e sostituiscili con niente "" (ad es. partendo da 333 - 5555, si ottiene 3335555)
+            )
+        )
+    )
+    
+def fill_null(col_name, fill_value):
+    return (
+        F.when(F.col(col_name).isNull(), F.lit(fill_value) )
+        .otherwise(F.col(col_name) )
+    )
