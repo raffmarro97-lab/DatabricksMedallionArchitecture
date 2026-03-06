093f73ae0dfa9e148b78d9c4f4d7cc8e8a8de45b
raffmarro97-lab <raffmarro97@gmail.com>
2026-03-06 00:12:27 +0000

    Gold Aggregated Transformations

---
+++ medallion_dlt_breweries/transformations/04agg_breweries.py
@@ -0,0 +1,21 @@
+from pyspark import pipelines as dp
+from pyspark.sql import functions as F
+
+@dp.table(
+    name = "workspace.pipeline_breweries.agg_breweries",
+    comment = "Aggregated breweries - Numero di breweries per stato"
+)
+def agg_breweries():
+    df =  spark.read.table("gold_breweries")
+
+    return (
+        df
+            .where(F.col("__END_AT").isNull()) # solo record correnti SCD2
+            .groupBy("state")
+            .agg(
+                F.count("brewery_sk").alias("num_breweries"),
+                F.countDistinct("brewery_type").alias("num_types")
+            )
+            .orderBy(F.col("num_breweries").desc())
+
+    )
