90b59f4ef6a27661b93a2de96c7f013c5f3851b6
raffmarro97-lab <raffmarro97@gmail.com>
2026-03-06 00:12:10 +0000

    Gold Modeling and SCD Type 2

---
+++ medallion_dlt_breweries/transformations/03gold_breweries.py
@@ -0,0 +1,22 @@
+from pyspark import pipelines as dp
+from pyspark.sql.functions import col
+
+
+# Please edit the sample below
+@dp.view
+def silver_view_breweries_stream():
+    return spark.readStream.table("silver_breweries")
+
+dp.create_streaming_table(
+    name = "workspace.pipeline_breweries.gold_breweries",
+    comment = "SCD Type 2 - Storico delle modifiche"
+)
+
+dp.create_auto_cdc_flow(
+    target = "gold_breweries",
+    source = "silver_view_breweries_stream",
+    keys = ["brewery_sk"],
+    sequence_by = col("ingestion_ts"),
+    stored_as_scd_type = 2,
+    except_column_list = ["ingestion_ts"]
+)
