7a27bdbb8c71f60d52e6220a9d025a6ca5d7e476
raffmarro97-lab <raffmarro97@gmail.com>
2026-04-13 23:05:13 +0000

    fix(pass_config and bronze) modified schema and name config delta table

--- medallion_dlt_breweries/medallion_dlt_breweries_bronze_silver/transformations/01bronze_breweries.py
+++ medallion_dlt_breweries/medallion_dlt_breweries_bronze_silver/transformations/01bronze_breweries.py
@@ -20,8 +20,8 @@ empty_schema = StructType(
     StructField("state_province", StringType(), True),
     StructField("postal_code",    StringType(), True),
     StructField("country",        StringType(), True),
-    StructField("longitude",      StringType(), True),
-    StructField("latitude",       StringType(), True),
+    StructField("longitude",      DoubleType(), True),
+    StructField("latitude",       DoubleType(), True),
     StructField("phone",          StringType(), True),
     StructField("website_url",    StringType(), True),
     StructField("state",          StringType(), True),
@@ -53,7 +53,7 @@ def bronze_breweries():
   data = response.json()
 
   if data:
-    df_raw = spark.createDataFrame(data)
+    df_raw = spark.createDataFrame(data, schema = empty_schema)
     return df_raw
   else:
     return spark.createDataFrame([], schema = empty_schema)
