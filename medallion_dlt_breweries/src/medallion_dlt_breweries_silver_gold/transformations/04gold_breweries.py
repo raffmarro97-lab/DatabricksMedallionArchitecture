4b505ca79d5c92f826ce041a644e73a5d5dc7aed
Raffaele Marro <raffmarro97@gmail.com>
2026-07-20 18:28:54 +0200

    Fix Python linting issues and modify max-line-lenght .flake8 file

--- medallion_dlt_breweries/src/medallion_dlt_breweries_silver_gold/transformations/04gold_breweries.py
+++ medallion_dlt_breweries/src/medallion_dlt_breweries_silver_gold/transformations/04gold_breweries.py
@@ -7,23 +7,26 @@ schema_name = spark.conf.get("breweries.schema")
 silver_table = f"{catalog}.{schema_name}.silver_breweries"
 
 # Please edit the sample below
+
+
 @dp.table
 def silver_breweries_stream():
     return (spark.readStream
-                            .option("skipChangeCommits", "true")
-                            .table(silver_table)
+            .option("skipChangeCommits", "true")
+            .table(silver_table)
             )
 
+
 dp.create_streaming_table(
-    name = "gold_breweries",
-    comment = "SCD Type 2 - Storico delle modifiche"
+    name="gold_breweries",
+    comment="SCD Type 2 - Storico delle modifiche"
 )
 
 dp.create_auto_cdc_flow(
-    target = "gold_breweries",
-    source = "silver_breweries_stream",
-    keys = ["id"],
-    sequence_by = col("ingestion_ts"),
-    stored_as_scd_type = 2,
-    except_column_list = ["ingestion_ts"]
+    target="gold_breweries",
+    source="silver_breweries_stream",
+    keys=["id"],
+    sequence_by=col("ingestion_ts"),
+    stored_as_scd_type=2,
+    except_column_list=["ingestion_ts"]
 )
