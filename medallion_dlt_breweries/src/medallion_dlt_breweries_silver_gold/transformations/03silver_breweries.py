c64ae1adbf6ba0bec68ee5282fccc3cf139a1c33
Raffaele Marro <raffmarro97@gmail.com>
2026-07-27 16:10:30 +0200

    Add data quality expectattions to Silver pipeline

--- medallion_dlt_breweries/src/medallion_dlt_breweries_silver_gold/transformations/03silver_breweries.py
+++ medallion_dlt_breweries/src/medallion_dlt_breweries_silver_gold/transformations/03silver_breweries.py
@@ -13,6 +13,26 @@ silver_staging_table = f"{catalog}.{schema_name}.silver_staging_breweries"
     name="silver_breweries",
     comment="Cleaning the bronze table, and add ingestion_ts"
 )
+@dp.expect_or_fail(
+    "valid_ingestion_timestamp",
+    "id is not NULL"
+)
+@dp.expect_or_drop(
+    "valid_country",
+    "country = 'United States'",
+)
+@dp.expect_or_drop(
+    "valid_primary_address",
+    "address_1 IS NOT NULL"
+)
+@dp.expect(
+    "valid_brewery_name",
+    "name IS NOT NULL AND trim(name) <> ''",
+)
+@dp.expect(
+    "valid_brewery_type",
+    "brewery_type IS NOT NULL",
+)
 def silver_breweries():
     source_df = spark.read.table(silver_staging_table)
     return transform_silver_breweries(source_df)
